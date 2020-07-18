4d403e840aded3328c866683d517d83405af4e3f
TuxFan77 <briancross@shaw.ca>
2020-07-17 20:46:41 -0600

    Changed media constraints

--- src/utils/getMediaStream.js
+++ src/utils/getMediaStream.js
@@ -1,21 +1,20 @@
 async function getMediaStream() {
   const mediaStream = await navigator.mediaDevices.getUserMedia({
     audio: true,
-    video: true
+    video: {
+      width: { min: 640, ideal: 1920 },
+      height: { min: 480, ideal: 1080 }
+    }
   });
 
-  // const videoTrack = mediaStream.getVideoTracks()[0];
+  const videoTrack = mediaStream.getVideoTracks()[0];
 
   // Firefox doesn't support MediaStreamTrack.getCapabilities()
   // https://bugzilla.mozilla.org/show_bug.cgi?id=1179084
-  // if (videoTrack.getCapabilities) {
-  //   const { width, height } = videoTrack.getCapabilities();
-
-  //   videoTrack.applyConstraints({
-  //     width: width.max,
-  //     height: height.max
-  //   });
-  // }
+  if (videoTrack.getCapabilities) {
+    const { width, height } = videoTrack.getCapabilities();
+    console.log(`Actual width: ${width.max}\nActual height: ${height.max}`);
+  }
 
   return mediaStream;
 }
