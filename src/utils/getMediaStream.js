3bb0e3bf3f5c71ed2b63a2e122efe5ecb76b3e7d
TuxFan77 <briancross@shaw.ca>
2020-07-20 18:14:40 -0600

    Changed media constraints to default for now to allow mobile Safari to work

--- src/utils/getMediaStream.js
+++ src/utils/getMediaStream.js
@@ -1,30 +1,17 @@
 async function getMediaStream() {
-  const query = window.matchMedia("(orientation: portrait)");
-  console.log(query);
-
-  const portraitVideoConstraints = {
-    width: 1080,
-    height: 1920
-  };
-
-  const landscapeVideoConstraints = {
-    width: 1920,
-    height: 1080
-  };
-
   const mediaStream = await navigator.mediaDevices.getUserMedia({
     audio: true,
-    video: query.matches ? portraitVideoConstraints : landscapeVideoConstraints
+    video: true
   });
 
-  const videoTrack = mediaStream.getVideoTracks()[0];
+  // const videoTrack = mediaStream.getVideoTracks()[0];
 
   // Firefox doesn't support MediaStreamTrack.getCapabilities()
   // https://bugzilla.mozilla.org/show_bug.cgi?id=1179084
-  if (videoTrack.getCapabilities) {
-    const { width, height } = videoTrack.getCapabilities();
-    console.log(`Actual max camera resolution: ${width.max} x ${height.max}`);
-  }
+  // if (videoTrack.getCapabilities) {
+  //   const { width, height } = videoTrack.getCapabilities();
+  //   console.log(`Actual max camera resolution: ${width.max} x ${height.max}`);
+  // }
 
   return mediaStream;
 }
