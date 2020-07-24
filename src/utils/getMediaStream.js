7b5e78bfc397c33115f96d018ea5df8cda0395d3
TuxFan77 <briancross@shaw.ca>
2020-07-23 23:01:35 -0600

    More verbose logging in getMediaStream

--- src/utils/getMediaStream.js
+++ src/utils/getMediaStream.js
@@ -1,11 +1,16 @@
 async function getMediaStream() {
-  console.log(navigator.platform);
+  console.log("getMediaStream");
+
+  const { platform } = navigator;
+  console.log(`platform: ${platform}`);
 
   const constraints = { audio: true };
 
-  if (["iPhone", "iPad", "iPod"].includes(navigator.platform)) {
+  if (platform && ["iPhone", "iPad", "iPod"].includes(platform)) {
+    console.log("iOS");
     constraints.video = true;
   } else {
+    console.log("not iOS");
     constraints.video = {
       width: { min: 640, ideal: 1280, max: 1920 },
       height: { min: 480, ideal: 720, max: 1080 }
