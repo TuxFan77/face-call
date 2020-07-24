8b0fe8337403ed7f7014944692e0f02152d02835
TuxFan77 <briancross@shaw.ca>
2020-07-24 12:23:31 -0600

    Removed temporary log statements

--- src/utils/getMediaStream.js
+++ src/utils/getMediaStream.js
@@ -7,10 +7,8 @@ async function getMediaStream() {
   const constraints = { audio: true };
 
   if (platform && ["iPhone", "iPad", "iPod"].includes(platform)) {
-    console.log("iOS");
     constraints.video = true;
   } else {
-    console.log("not iOS");
     constraints.video = {
       width: { min: 640, ideal: 1280, max: 1920 },
       height: { min: 480, ideal: 720, max: 1080 }
