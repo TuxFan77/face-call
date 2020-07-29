8a06a29919fad691247638f3183241b86683253c
TuxFan77 <briancross@shaw.ca>
2020-07-28 21:10:24 -0600

    Moved all WebRTC code into its own object

---
+++ src/components/video-call/VideoCall.js
@@ -0,0 +1,240 @@
+import {
+  initWebSocket,
+  sendToServer,
+  closeWebSocket
+} from "../../web-socket/webSocket";
+import getIceServers from "../../web-rtc/getIceServers";
+
+import { v4 as uuidv4 } from "uuid";
+
+function VideoCall(localVideo, remoteVideo, role) {
+  this.role = role;
+  this.sendToServer = sendToServer;
+  const cameras = [];
+  let peerConnection = null;
+  const userId = uuidv4();
+
+  // Starts a video call
+  this.start = async function () {
+    try {
+      await initLocalVideo();
+      await getCameras();
+      await initWebSocket(
+        handleVideoOfferMessage,
+        handleVideoAnswerMessage,
+        handleNewRemoteICECandidate,
+        this.endCall
+      );
+
+      this.sendToServer({
+        type: "set-user-id",
+        userId
+      });
+
+      if (role === "caller") startCall();
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  // Initialize the local video DOM element with the webcam feed
+  async function initLocalVideo() {
+    console.log("initLocalVideo");
+    const stream = await getMediaStream();
+    console.log(stream);
+    localVideo.current.srcObject = stream;
+    localVideo.current.play();
+  }
+
+  // Enumerates the cameras on the device and adds them to the cameras array
+  function getCameras() {
+    console.log("getCameras");
+    return navigator.mediaDevices
+      .enumerateDevices()
+      .then(devices => {
+        devices.forEach(device => {
+          if (device.kind === "videoinput") {
+            console.log(device.label);
+            cameras.push(device.label);
+          }
+        });
+      })
+      .catch(err => err);
+  }
+
+  // Gets the media stream
+  async function getMediaStream() {
+    console.log("getMediaStream");
+
+    const { platform } = navigator;
+    console.log(`platform: ${platform}`);
+
+    const constraints = { audio: true };
+
+    if (platform && ["iPhone", "iPad", "iPod"].includes(platform)) {
+      constraints.video = true;
+    } else {
+      constraints.video = {
+        width: { min: 640, ideal: 1280, max: 1920 },
+        height: { min: 480, ideal: 720, max: 1080 }
+      };
+    }
+
+    return await navigator.mediaDevices.getUserMedia(constraints);
+  }
+
+  // Start a video call
+  // This function is called if we're the peer that's initiaiting the call
+  async function startCall() {
+    console.log("startCall");
+
+    try {
+      peerConnection = new RTCPeerConnection(await getIceServers());
+      peerConnection.onnegotiationneeded = handleNegotiationNeeded;
+      peerConnection.ontrack = handleAddTrack;
+      peerConnection.onicecandidate = handleNewLocalICECandidate;
+
+      const localStream = localVideo.current.srcObject;
+      localStream
+        .getTracks()
+        .forEach(track => peerConnection.addTrack(track, localStream));
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  // Handles the negotiationneeded event on the peerConnection object
+  // Fires when tracks are added to the peerConnection that initiated the call
+  async function handleNegotiationNeeded() {
+    console.log("handleNegotiationNeeded");
+
+    try {
+      const offer = await peerConnection.createOffer();
+      await peerConnection.setLocalDescription(offer);
+
+      sendToServer({
+        type: "video-offer",
+        userId,
+        payload: offer
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  // Handles incoming video offer message over the web socket connection
+  // Fires if we're the callee and we receive an offer from the caller
+  async function handleVideoOfferMessage(offer) {
+    console.log("handleVideoOfferMessage");
+
+    try {
+      peerConnection = new RTCPeerConnection(await getIceServers());
+      peerConnection.ontrack = handleAddTrack;
+      peerConnection.onicecandidate = handleNewLocalICECandidate;
+
+      const localStream = localVideo.current.srcObject;
+
+      localStream
+        .getTracks()
+        .forEach(track => peerConnection.addTrack(track, localStream));
+
+      await peerConnection.setRemoteDescription(
+        new RTCSessionDescription(offer)
+      );
+      const answer = await peerConnection.createAnswer();
+      await peerConnection.setLocalDescription(answer);
+
+      sendToServer({
+        type: "video-answer",
+        userId,
+        payload: answer
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  // Handles incoming video answer message over the web socket connection
+  // Fires if we're the caller and we're receiving an answer to our offer
+  async function handleVideoAnswerMessage(answer) {
+    console.log("handleVideoAnswerMessage");
+
+    try {
+      await peerConnection.setRemoteDescription(
+        new RTCSessionDescription(answer)
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  // Handles incoming tracks from the remote peer
+  // Adds the tracks to the remote video player
+  // Fires when remote tracks come in
+  function handleAddTrack(e) {
+    console.log("handleAddTrack");
+    if (remoteVideo.current.srcObject) return;
+    remoteVideo.current.srcObject = e.streams[0];
+    console.log(e);
+  }
+
+  // Sends locally generated ICE candidates to the remote peer
+  function handleNewLocalICECandidate(e) {
+    console.log("handleNewLocalICECandidate");
+    if (!e.candidate) {
+      console.log("NO LOCAL CANDIDATE");
+      return;
+    }
+    sendToServer({
+      type: "new-ice-candidate",
+      userId,
+      payload: e.candidate
+    });
+  }
+
+  // Receives ICE candidates from the remote peer
+  // and adds them to the local peer connection
+  async function handleNewRemoteICECandidate(candidate) {
+    console.log("handleNewRemoteICECandidate");
+    if (!peerConnection) {
+      console.log("NO REMOTE CANDIDATE");
+      return;
+    }
+    try {
+      await peerConnection.addIceCandidate(candidate);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  // End the call and cleans up resources
+  this.endCall = function () {
+    console.log("endCall");
+
+    sendToServer({
+      type: "end-call",
+      userId,
+      payload: ""
+    });
+
+    closeWebSocket();
+
+    if (localVideo.current.srcObject) {
+      localVideo.current.pause();
+      localVideo.current.srcObject.getTracks().forEach(track => track.stop());
+      localVideo.current.removeAttribute("src");
+      localVideo.current.removeAttribute("srcObject");
+    }
+
+    if (remoteVideo.current.srcObject) {
+      // remoteVideo.current.pause();
+      remoteVideo.current.srcObject.getTracks().forEach(track => track.stop());
+      remoteVideo.current.removeAttribute("src");
+      remoteVideo.current.removeAttribute("srcObject");
+    }
+
+    peerConnection = null;
+  };
+}
+
+export default VideoCall;
--- src/components/video-call/VideoCallUI.js
+++ src/components/video-call/VideoCallUI.js
@@ -1,67 +1,39 @@
-import React, { useRef, useEffect } from "react";
-import { v4 as uuidv4 } from "uuid";
+import React, { useState, useEffect, useRef } from "react";
 
+import VideoCall from "./VideoCall";
 import VideoPageContainer from "./VideoPageContainer";
 import LocalVideo from "./LocalVideo";
 import ControlsBar from "./ControlsBar/ControlsBar";
 import RemoteVideo from "./RemoteVideo";
 import { useQuery } from "../../hooks/hooks";
-import getMediaStream from "../../utils/getMediaStream";
 import {
   videoPageVariants,
   pageTransition
 } from "../../animation/pageTransition";
-import {
-  initWebSocket,
-  sendToServer,
-  closeWebSocket
-} from "../../web-socket/webSocket";
-import getIceServers from "../../web-rtc/getIceServers";
 
-let peerConnection, localVideo, remoteVideo;
-
-const userId = uuidv4();
-// const callId = uuidv4();
-
-const VideoCall = () => {
-  let isCaller = useRef(false);
-  const cameras = useRef([]);
-  localVideo = useRef(null);
-  remoteVideo = useRef(null);
+const VideoCallUI = () => {
+  const localVideo = useRef(null);
+  const remoteVideo = useRef(null);
+  let role = "";
 
   const query = useQuery();
   if (query.has("isCaller")) {
     if (query.get("isCaller") === "true") {
-      isCaller.current = true;
+      role = "caller";
     }
   }
 
+  const [videoCall, setVideoCall] = useState(
+    new VideoCall(localVideo, remoteVideo, role)
+  );
+
   useEffect(() => {
-    (async () => {
-      try {
-        await initLocalVideo();
-        const cameraList = await getCameras();
-        cameraList.forEach(camera => cameras.current.push(camera));
-
-        await initWebSocket(
-          handleVideoOfferMessage,
-          handleVideoAnswerMessage,
-          handleNewRemoteICECandidate,
-          endCall
-        );
-
-        sendToServer({
-          type: "set-user-id",
-          userId
-        });
-
-        if (isCaller) startCall();
-      } catch (error) {
-        console.log(error);
-      }
-    })();
-    return () => endCall();
-  }, []);
+    videoCall.start();
+    return () => {
+      videoCall.endCall();
+      setVideoCall(null);
+    };
+  }, [videoCall]);
 
   const handleControlsBarButtonClick = button => {
     switch (button) {
@@ -80,7 +52,7 @@ const VideoCall = () => {
 
       case "end":
         console.log("end call button clicked");
-        endCall();
+        videoCall.endCall();
         break;
 
       default:
@@ -106,181 +78,4 @@ const VideoCall = () => {
   );
 };
 
-export default VideoCall;
-
-// Initialize the local video element with the webcam feed
-async function initLocalVideo() {
-  console.log("initLocalVideo");
-  const stream = await getMediaStream();
-  console.log(stream);
-  localVideo.current.srcObject = stream;
-  localVideo.current.play();
-}
-
-// Returns an array of camera device labels
-function getCameras() {
-  console.log("getCameras");
-  const deviceLabels = [];
-  return navigator.mediaDevices
-    .enumerateDevices()
-    .then(devices => {
-      devices.forEach(device => {
-        if (device.kind === "videoinput") {
-          deviceLabels.push(device.label);
-        }
-      });
-      return deviceLabels;
-    })
-    .catch(err => err);
-}
-
-// Start a video call
-// This function is called if we're the peer that's initiaiting the call
-async function startCall() {
-  console.log("startCall");
-
-  try {
-    peerConnection = new RTCPeerConnection(await getIceServers());
-    peerConnection.onnegotiationneeded = handleNegotiationNeeded;
-    peerConnection.ontrack = handleAddTrack;
-    peerConnection.onicecandidate = handleNewLocalICECandidate;
-
-    const localStream = localVideo.current.srcObject;
-    localStream
-      .getTracks()
-      .forEach(track => peerConnection.addTrack(track, localStream));
-  } catch (error) {
-    console.log(error);
-  }
-}
-
-// Handles the negotiationneeded event on the peerConnection object
-// Fires when tracks are added to the peerConnection that initiated the call
-async function handleNegotiationNeeded() {
-  console.log("handleNegotiationNeeded");
-
-  try {
-    const offer = await peerConnection.createOffer();
-    await peerConnection.setLocalDescription(offer);
-
-    sendToServer({
-      type: "video-offer",
-      userId,
-      payload: offer
-    });
-  } catch (error) {
-    console.log(error);
-  }
-}
-
-// Handles incoming video offer message over the web socket connection
-// Fires if we're the callee and we receive an offer from the caller
-async function handleVideoOfferMessage(offer) {
-  console.log("handleVideoOfferMessage");
-
-  try {
-    peerConnection = new RTCPeerConnection(await getIceServers());
-    peerConnection.ontrack = handleAddTrack;
-    peerConnection.onicecandidate = handleNewLocalICECandidate;
-
-    const localStream = localVideo.current.srcObject;
-
-    localStream
-      .getTracks()
-      .forEach(track => peerConnection.addTrack(track, localStream));
-
-    await peerConnection.setRemoteDescription(new RTCSessionDescription(offer));
-    const answer = await peerConnection.createAnswer();
-    await peerConnection.setLocalDescription(answer);
-
-    sendToServer({
-      type: "video-answer",
-      userId,
-      payload: answer
-    });
-  } catch (error) {
-    console.log(error);
-  }
-}
-
-// Handles incoming video answer message over the web socket connection
-// Fires if we're the caller and we're receiving an answer to our offer
-async function handleVideoAnswerMessage(answer) {
-  console.log("handleVideoAnswerMessage");
-
-  try {
-    await peerConnection.setRemoteDescription(
-      new RTCSessionDescription(answer)
-    );
-  } catch (error) {
-    console.log(error);
-  }
-}
-
-// Handles incoming tracks from the remote peer
-// Adds the tracks to the remote video player
-// Fires when remote tracks come in
-function handleAddTrack(e) {
-  console.log("handleAddTrack");
-  if (remoteVideo.current.srcObject) return;
-  remoteVideo.current.srcObject = e.streams[0];
-  console.log(e);
-}
-
-// Sends locally generated ICE candidates to the remote peer
-function handleNewLocalICECandidate(e) {
-  console.log("handleNewLocalICECandidate");
-  if (!e.candidate) {
-    console.log("NO LOCAL CANDIDATE");
-    return;
-  }
-  sendToServer({
-    type: "new-ice-candidate",
-    userId,
-    payload: e.candidate
-  });
-}
-
-// Receives ICE candidates from the remote peer
-// and adds them to the local peer connection
-async function handleNewRemoteICECandidate(candidate) {
-  console.log("handleNewRemoteICECandidate");
-  if (!peerConnection) {
-    console.log("NO REMOTE CANDIDATE");
-    return;
-  }
-  try {
-    await peerConnection.addIceCandidate(candidate);
-  } catch (error) {
-    console.log(error);
-  }
-}
-
-// End the call and cleans up resources
-function endCall() {
-  console.log("endCall");
-
-  sendToServer({
-    type: "end-call",
-    userId,
-    payload: ""
-  });
-
-  closeWebSocket();
-
-  if (localVideo.current.srcObject) {
-    localVideo.current.pause();
-    localVideo.current.srcObject.getTracks().forEach(track => track.stop());
-    localVideo.current.removeAttribute("src");
-    localVideo.current.removeAttribute("srcObject");
-  }
-
-  if (remoteVideo.current.srcObject) {
-    // remoteVideo.current.pause();
-    remoteVideo.current.srcObject.getTracks().forEach(track => track.stop());
-    remoteVideo.current.removeAttribute("src");
-    remoteVideo.current.removeAttribute("srcObject");
-  }
-
-  peerConnection = null;
-}
+export default VideoCallUI;
