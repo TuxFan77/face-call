89de3408cdd63ac8bef921446092258bc6dfe704
TuxFan77 <briancross@shaw.ca>
2020-08-07 22:03:02 -0600

    switched to form input

--- src/components/name-entry/NameEntryCard.js
+++ src/components/name-entry/NameEntryCard.js
@@ -1,10 +1,37 @@
-import React, { useState, useRef } from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 
 import Card from "../../styles/global/Card";
 import CardContent from "../../styles/global/CardContent";
 import Input from "../../styles/global/Input";
-import LargeButton from "../../styles/global/LargeButton";
+
+import {
+  primaryGradient,
+  primaryFontColor
+} from "../../styles/constants/colors";
+import { mainFontFamily, primaryFontSize } from "../../styles/constants/fonts";
+
+const Button = styled.input`
+  background: ${primaryGradient};
+  border: none;
+  box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.25);
+  text-align: center;
+  cursor: pointer;
+  padding: 0.15em 0;
+  font-size: ${primaryFontSize};
+  font-family: ${mainFontFamily};
+  color: ${primaryFontColor};
+  width: 100%;
+
+  &:focus {
+    outline-style: dotted;
+  }
+
+  &:active,
+  &:visited {
+    color: ${primaryFontColor};
+  }
+`;
 
 const Heading = styled.h1`
   margin-bottom: 3rem;
@@ -17,33 +44,21 @@ const InputField = styled(Input)`
 const NameEntryCard = ({ name, handleNameEntry }) => {
   const [callerName, setName] = useState(name);
 
-  const button = useRef(null);
-
-  function handleEnterKey() {
-    button.current.click();
-  }
-
   return (
     <Card>
       <CardContent>
         <Heading>Step 1</Heading>
-        <h2>Enter your name:</h2>
-        <InputField
-          type="text"
-          value={callerName}
-          onChange={e => setName(e.target.value)}
-          onKeyUp={e => {
-            if (e.keyCode === 13) handleEnterKey();
-          }}
-        />
-        <LargeButton
-          to="/enterRecipients"
-          ref={button}
-          disabled={callerName.length === 0}
-          onClick={() => handleNameEntry(callerName)}
-        >
-          Next
-        </LargeButton>
+        <form>
+          <label>
+            <h2>Enter your name:</h2>
+          </label>
+          <InputField
+            type="text"
+            value={callerName}
+            onChange={e => setName(e.target.value)}
+          />
+          <Button type="submit" value="Next" />
+        </form>
       </CardContent>
     </Card>
   );
