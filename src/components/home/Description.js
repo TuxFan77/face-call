5b28b04bef70dca3f8ff9d10b13cacdd3b11d925
TuxFan77 <briancross@shaw.ca>
2020-08-14 20:05:33 -0600

    moved style code into main component

--- src/components/home/Description.js
+++ src/components/home/Description.js
@@ -1,6 +1,12 @@
 import React from "react";
+import styled from "styled-components";
 
-import { DescriptionContainer, List } from "./DescriptionStyles";
+import { accentColor } from "../../styles/constants/colors";
+import {
+  bulletDiameter,
+  listMarginTopBottom,
+  listFontSize
+} from "../../styles/constants/sizes";
 import Button from "../../styles/global/Button";
 
 const Description = () => {
@@ -17,5 +23,46 @@ const Description = () => {
     </DescriptionContainer>
   );
 };
+
+export const DescriptionContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: ${props => (props.alignLeft ? "flex-start" : "center")};
+  justify-content: space-between;
+
+  h1 {
+    text-align: center;
+  }
+`;
+
+export const List = styled.ul`
+  text-align: center;
+
+  li {
+    margin: ${listMarginTopBottom} 0;
+    font-size: ${listFontSize};
+
+    &:nth-of-type(2) {
+      position: relative;
+      &::before,
+      &::after {
+        content: "";
+        height: ${bulletDiameter};
+        width: ${bulletDiameter};
+        border-radius: 50%;
+        background-color: ${accentColor};
+        display: block;
+        position: absolute;
+        left: calc(50% - ${bulletDiameter} / 2);
+      }
+      &::before {
+        top: calc(-${listMarginTopBottom} / 2 - ${bulletDiameter} / 2);
+      }
+      &::after {
+        bottom: calc(-${listMarginTopBottom} / 2 - ${bulletDiameter} / 2);
+      }
+    }
+  }
+`;
 
 export default Description;
