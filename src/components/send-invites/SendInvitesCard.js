0d923f631dd6258f6b8325deecf23494795e680b
Brian Cross <briancross@shaw.ca>
2020-11-26 11:24:04 -0700

    Remove SMS / email code.

--- src/components/send-invites/SendInvitesCard.js
+++ src/components/send-invites/SendInvitesCard.js
@@ -1,50 +1,31 @@
 import React from "react";
 import styled from "styled-components";
-import { v4 as uuidv4 } from "uuid";
+// import { v4 as uuidv4 } from "uuid";
 
 import Card from "../common/Card";
 import CardContent from "../common/CardContent";
 import LinkButton from "../common/LinkButton";
-import sendSms from "../../communication/sendSms";
+import generateRandomHexString from "../../utils/generateRandomHexString";
 
-const room = uuidv4();
+// const room = uuidv4();
+const ROOM_STRING_LENGTH = 6;
+const room = generateRandomHexString(ROOM_STRING_LENGTH);
 const PATH = `/videoCall`;
 
 const Heading = styled.h1`
   margin-bottom: 3rem;
 `;
 
-const SendInvitesCard = ({ caller, recipient }) => {
-  const message = `${caller} has sent you a video call invitation.
-
-Click here to join the call:
-
-${window.origin}${PATH}/${room}`;
-
-  function handleClick() {
-    if (recipient.type === "sms") {
-      return sendSms({
-        message,
-        to: recipient.contact,
-      });
-    }
-
-    if (recipient.type === "email") {
-      return console.log(
-        `Contacting recipient via ${recipient.type} at ${recipient.contact}: ${message}`
-      );
-    }
-  }
-
+const SendInvitesCard = () => {
   return (
     <Card>
       <CardContent>
-        <Heading>Step 3</Heading>
-        <h2>Send invite and start your call.</h2>
-        <p>Sending video chat invite link to {recipient.contact}</p>
-        <LinkButton onClick={handleClick} to={`${PATH}/${room}`}>
-          Go!
-        </LinkButton>
+        <Heading>Send Video Call Link</Heading>
+        <p>
+          Send this link to the other participant and then click the Go button.
+        </p>
+        <p>{`${window.origin}${PATH}/${room}`}</p>
+        <LinkButton to={`${PATH}/${room}`}>Go!</LinkButton>
       </CardContent>
     </Card>
   );
